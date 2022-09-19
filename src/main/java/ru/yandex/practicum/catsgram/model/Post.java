9b39b55050cda5bd4f868df39cfaa162fbf4e1e1
Alexey <sepens@yandex.ru>
2022-09-19 21:19:27 +0300

    спринт 10: переменная пути

--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -8,11 +8,13 @@ import java.time.Instant;
 public class Post {
 
     private final String author; // автор
+    private Integer id;
     private final Instant creationDate = Instant.now(); // дата создания
     private String description; // описание
     private String photoUrl; // url-адрес фотографии
 
-    public Post(String author, String description, String photoUrl) {
+    public Post(Integer id, String author, String description, String photoUrl) {
+        this.id = id;
         this.author = author;
         this.description = description;
         this.photoUrl = photoUrl;
--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.catsgram.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.yandex.practicum.catsgram.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -18,4 +20,11 @@ public class PostService {
         posts.add(post);
         return post;
     }
+
+    public Optional<Post> findById(@PathVariable int postId) {
+        return posts.stream()
+                .filter(x -> x.getId() == postId)
+                .findFirst();
+    }
+
 }
